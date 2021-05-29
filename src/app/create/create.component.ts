2be00e347cb1ce1d12520cd1525fdfdfb805deb8
KovacsLevente <37476260+KovacsLevente@users.noreply.github.com>
2021-05-29 20:00:56 +0200

    Create

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-
-  constructor() { }
+  data:any = [];
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.data = localStorage.getItem("data");
+    this.data = JSON.parse(this.data);
   }
 
+  Create(ip:string, host:string, url:string){
+    this.data.push([ip,host,url]);
+    localStorage.setItem("data", JSON.stringify(this.data));
+    this.router.navigate(['/list']);
+  }
 }
